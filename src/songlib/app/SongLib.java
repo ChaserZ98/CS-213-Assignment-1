8f12ddb143acad889185752defb8a39ed1efb24b
ChaserZ98 <feiyuzheng98@gmail.com>
2020-09-28 00:01:52 -0400

    update

--- src/songlib/app/SongLib.java
+++ src/songlib/app/SongLib.java
@@ -2,13 +2,22 @@ package songlib.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SongLib extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Fahrenheit-Celsius");
+        primaryStage.setResizable(false);
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/song/view/songlib.fxml"));
+        loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
+        GridPane root = (GridPane)loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     public static void main(String[] args){
         launch(args);
---
+++ src/songlib/view/SongLibController.java
@@ -0,0 +1,4 @@
+package songlib.view;
+
+public class SongLibController {
+}
